d19cf679ab810be5a54f379e8727b4f13f6a11fa
logan Maloney <shakenasty13@gmail.com>
2021-10-22 09:13:21 -0500

    initial commit

---
+++ variablesExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variablesExercise
+{
+    class Program
+    {
+        private static bool True;
+        private static char B;
+
+        static void Main(string[] args)
+        {
+            string catName ="Baker";
+            int catAge = 2; 
+            char firstInitial = B;
+            bool likesTreats = True;
+            Console.WriteLine($"My cat's name is {catName}, He is {catAge}, his first name starts with {firstInitial}");
+
+        }
+    }
+}
