525e1eade8626c3ea13d9f56dd45a0094ddbfbce
logan Maloney <shakenasty13@gmail.com>
2021-10-22 09:57:12 -0500

    initial commit

--- variablesExercise/Program.cs
+++ variablesExercise/Program.cs
@@ -15,9 +15,11 @@ namespace variablesExercise
         {
             string catName ="Baker";
             int catAge = 2; 
-            char firstInitial = B;
+            char firstInitial = 'B';
             bool likesTreats = True;
-            Console.WriteLine($"My cat's name is {catName}, He is {catAge}, his first name starts with {firstInitial}");
+            double catHeight = 1.3;
+            decimal catWeight = 10.2m; 
+            Console.WriteLine($"My cat's name is {catName}, He is {catAge}, his first name starts with {firstInitial}, My cat's height is {catHeight} feet tall, My cat weighs {catWeight} pounds.");
 
         }
     }
